9e7e74f0d3f01f7e28905f24050bca9822849f72
Hiro Asari <asari.ruby@gmail.com>
2015-08-06 16:53:10 -0400

    More debugging

--- lib/travis/build/env/builtin.rb
+++ lib/travis/build/env/builtin.rb
@@ -8,7 +8,8 @@ module Travis
         def_delegators :data, :build, :job, :repository, :pull_request
 
         def vars
-          puts job
+          puts data.keys
+
           env_vars.map do |key, value|
             value = value.to_s.shellescape
             value = [BUILD_DIR, value].join('/') if key == :TRAVIS_BUILD_DIR
