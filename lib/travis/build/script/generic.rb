896b80bceae56457ad3597f763dfd4a34467773a
Tomas Carnecky <tomas.carnecky@gmail.com>
2015-08-15 13:15:16 +0200

    Add support for custom cache_slug in generic scripts

--- lib/travis/build/script/generic.rb
+++ lib/travis/build/script/generic.rb
@@ -3,6 +3,14 @@ module Travis
     class Script
       class Generic < Script
         DEFAULTS = {}
+
+        def cache_slug
+          if config[:cache].nil?
+            super
+          else
+            super << '-' << config[:cache].to_s
+          end
+        end
       end
     end
   end
